6a6a2e8eb2a656d7be60803239923330aa58b0f6
Kshilinaa <ksusha_2605@mail.ru>
2024-05-13 15:58:41 +0300

    init

---
+++ TeslaProject/TeslaProject/TeslaProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeslaProjectApp.swift
+//  TeslaProject
+//
+//  Created by Ксения Шилина on 13.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct TeslaProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
